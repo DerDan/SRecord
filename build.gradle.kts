a20826bb0317c7a6d82fdb7bd80c4db86b742c44
DFranke <franke@edf-online.de>
2021-04-29 23:00:22 +0200

    inline hint added

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,19 +92,20 @@ val generateLexer = task<GenerateLexer>("generateLexer") {
 
 
 tasks {
-    // Set the compatibility versions to 1.8
+    // Set the compatibility versions to 11
     withType<JavaCompile> {
         dependsOn(generateParser)
         dependsOn(generateParser)
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.freeCompilerArgs += "-Xjvm-default=enable"
     }
 
     withType<Detekt> {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     patchPluginXml {
---
+++ src/main/kotlin/SRecordTypeHintsPresentationFactory/SRecordTypeHintsPresentationFactory.kt
@@ -0,0 +1,27 @@
+import com.intellij.codeInsight.hints.presentation.InlayPresentation
+import com.intellij.codeInsight.hints.presentation.PresentationFactory
+
+@Suppress("UnstableApiUsage")
+class SRecordTypeHintsPresentationFactory(private val factory: PresentationFactory) {
+    fun textHint(t: String): InlayPresentation = factory.roundWithBackground(
+        listOf(text(t)).join()
+    )
+
+    private fun text(text: String?): InlayPresentation = factory.smallText(text ?: "?")
+
+    private fun List<InlayPresentation>.join(separator: String = ""): InlayPresentation {
+        if (separator.isEmpty()) {
+            return factory.seq(*toTypedArray())
+        }
+        val presentations = mutableListOf<InlayPresentation>()
+        var first = true
+        for (presentation in this) {
+            if (!first) {
+                presentations.add(text(separator))
+            }
+            presentations.add(presentation)
+            first = false
+        }
+        return factory.seq(*presentations.toTypedArray())
+    }
+}
