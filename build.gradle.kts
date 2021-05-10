cc18013654a8763bd6435b1f01eea9a765e1e6d3
Daniel Franke <franke@edf-online.de>
2021-05-10 13:56:27 +0200

    try source compatibility to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,22 +92,22 @@ val generateLexer = task<GenerateLexer>("generateLexer") {
 
 
 tasks {
-    // Set the compatibility versions to 1.8
+    // Set the compatibility versions to 11
     withType<JavaCompile> {
         dependsOn(generateParser)
         dependsOn(generateLexer)
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
     withType<KotlinCompile> {
         dependsOn(generateParser)
         dependsOn(generateLexer)
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
         kotlinOptions.freeCompilerArgs += "-Xjvm-default=enable"
     }
 
     withType<Detekt> {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     patchPluginXml {
